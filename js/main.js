71a138ccdaa973d06ca8c646b699f3412f3524d4
LakshayHasija <75783120+LakshayHasija@users.noreply.github.com>
2021-06-30 09:22:28 +0530

    Delete js directory

--- js/main.js
+++
@@ -1,9 +0,0 @@
-$('.slider-one')
-.not(".slick-intialized")
-.slick({
-    autoplay: true,
-    autoplaySpeed: 3000,
-    dots: true,
-    prevArrow: ".site-slider .slider-btn .prev",
-    nextArrow: ".site-slider .slider-btn .next",
-});
